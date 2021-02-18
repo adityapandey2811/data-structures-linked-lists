12aa1a92084694309bbae19f79c046c432f8dd60
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-02-18 18:38:13 +0530

    Create --EF-CloneLinkedListWithRandomPointer.c

---
+++ --EF-CloneLinkedListWithRandomPointer.c
@@ -0,0 +1,29 @@
+struct Node* copyRandomList(struct Node* head) {
+	struct Node *temp = head,*new,*link = NULL,*spare,*current;
+    while(temp){
+        new = (struct Node*)malloc(sizeof(struct Node));
+        new->val = temp->val;
+        new->next = NULL;
+        if(link == NULL){
+            link = new;
+        }
+        else{
+            spare->next = new;
+        }
+        spare = new;
+        spare->random = temp;
+        current = temp;
+        temp = temp->next;
+        current->next = spare;
+    }
+    new = link;
+    while(new){
+        if(new->random->random != NULL)
+            new->random = new->random->random->next;
+        else
+            new->random = NULL;
+        new = new->next;
+    }
+    
+    return link;
+}
